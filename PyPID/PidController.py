90514ff58bb76e381249990e4a404878f115a31e
andrewgoodman <andrewgoodman@live.com>
2016-08-31 15:52:35 -0500

    Added functions to change the variables

--- PyPID/PidController.py
+++ PyPID/PidController.py
@@ -6,6 +6,10 @@ class PIDController:
         global kp,ki,kd
         global lastTime
         global SampleTime
+        global Setpoint
+        global Input
+        Setpoint = 0
+        Input = 0
         lastTime = 0
         kp,ki,kd =[0,0,0]
         SampleTime = 1000
@@ -30,12 +34,25 @@ class PIDController:
             lastErr = error
             lastTime = now
           
-    def setTunings(self,Kp,Ki,Kd):
-        global kp,ki,kd
+    def setTunings(self,Kp,Ki,Kd,inputVar):
+        global kp,ki,kd,Input
+        Input = inputVar
         SampleTimeInSec = SampleTime/1000
         kp = Kp
         ki = Ki * SampleTimeInSec
         kd = Kd / SampleTimeInSec
+
+    def setSampleTime(newSampleTime):
+        global SampleTime
+        if newSampleTime > 0:
+            ratio = newSampleTime/SampleTime
+            ki *= ratio
+            kd /= ratio
+            SampleTime = newSampleTime
+
+    def setSetpoint(self, setpoint):
+        global Setpoint
+        Setpoint = setpoint
 
     def printMil(self):
         print(millis())
